117d8b59a33d4c95eb01d04180156c5c794716a0
Iago Moure Pazos <14797378+i40130@users.noreply.github.com>
2024-12-29 17:20:04 +0000

    Deploy Angular app to GitHub Pages

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-6SHD7VTL.mjs";import"./chunk-VCV7JGHV.mjs";import"./chunk-RYOIILMX.mjs";import"./chunk-PSJ3SOSO.mjs";import"./chunk-WXEPCE6T.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
